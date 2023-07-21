59b814557e218ffa501914bc02aa3072b0ce2394
Milad Nekofar <milad@nekofar.com>
2023-07-21 16:02:59 +0330

    refactor: update homepage content and translations
    The website's homepage content was updated to provide more relevant information for the users. This change included the removal of language selection links replaced by a welcoming message and further details about the purpose of the website. Alongside this, the translations across 'fa', 'en', and 'ar' locale files were updated to sync with the modified content. This change provides a more user-focused and content-rich initial page for the site visitors.

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -2,7 +2,6 @@ import {availableLocales, loadCatalog} from '@/utils/locales'
 import {Trans} from '@lingui/macro'
 import {GetStaticPropsContext} from 'next'
 import {Inter} from 'next/font/google'
-import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,25 +10,23 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div>
-        <ul>
-          <li>
-            <Link href="/ar" locale="ar">
-              Arabic: <Trans>Arabic</Trans>
-            </Link>
-          </li>
-          <li>
-            <Link href="/en" locale="en">
-              English: <Trans>English</Trans>
-            </Link>
-          </li>
-          <li>
-            <Link href="/fa" locale="fa">
-              Farsi: <Trans>Farsi</Trans>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <section className="mt-16 py-8">
+        <div className="container">
+          <div className="mx-auto max-w-xl">
+            <h1 className="mb-4 text-center text-4xl font-bold leading-tight text-gray-100">
+              <Trans>Building a Multi-Lingual Website</Trans>
+            </h1>
+            <p className="mb-8 text-center text-lg text-gray-200">
+              <Trans>
+                Welcome to our Next.js and Lingui demo site! Discover the power
+                of combining Next.js, a powerful React framework for building
+                server-side rendered applications, with Lingui, the ultimate
+                solution for multi-lingual support in your web projects.
+              </Trans>
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
