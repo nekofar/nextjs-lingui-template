9ae41e767f937e60f3833e5e0ed134cf29c7b3c0
Milad Nekofar <milad@nekofar.com>
2023-07-21 19:29:57 +0330

    fix: add page title and update locale files
    HTML head and page title have been added to the homepage. The title is set with localization support to display correctly in different languages. English, Arabic, and Farsi translations have been updated. This change enhances SEO and provides clearer context for users browsing multiple tabs.

--- src/pages/[locale]/index.tsx
+++ src/pages/[locale]/index.tsx
@@ -1,37 +1,46 @@
 import {LocaleSwitcher} from '@/components/locale-switcher'
 import {availableLocales, loadCatalog} from '@/utils/locales'
-import {Trans} from '@lingui/macro'
 import {GetStaticPropsContext} from 'next'
 import {Vazirmatn} from 'next/font/google'
+import Head from 'next/head'
+import {t, Trans} from "@lingui/macro";
 
 const vazirmatn = Vazirmatn({ subsets: ['latin', 'arabic'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${vazirmatn.className}`}
-    >
-      <section className="mt-16 py-8">
-        <div className="container">
-          <div className="mx-auto max-w-xl">
-            <h1 className="mb-4 text-center text-4xl font-bold leading-tight text-gray-100">
-              <Trans>Building a Multi-Lingual Website</Trans>
-            </h1>
-            <p className="mb-8 text-center text-lg text-gray-200">
-              <Trans>
-                Welcome to our Next.js and Lingui demo site! Discover the power
-                of combining Next.js, a powerful React framework for building
-                server-side rendered applications, with Lingui, the ultimate
-                solution for multi-lingual support in your web projects.
-              </Trans>
-            </p>
-            <div className="mb-4 text-center ">
-              <LocaleSwitcher />
+    <>
+      <Head>
+        <title>
+          {t`Next.js & Lingui: Building a Multi-Lingual Website`}
+        </title>
+      </Head>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${vazirmatn.className}`}
+      >
+        <section className="mt-16 py-8">
+          <div className="container">
+            <div className="mx-auto max-w-xl">
+              <h1 className="mb-4 text-center text-4xl font-bold leading-tight text-gray-100">
+                <Trans>Building a Multi-Lingual Website</Trans>
+              </h1>
+              <p className="mb-8 text-center text-lg text-gray-200">
+                <Trans>
+                  Welcome to our Next.js and Lingui demo site! Discover the
+                  power of combining Next.js, a powerful React framework for
+                  building server-side rendered applications, with Lingui, the
+                  ultimate solution for multi-lingual support in your web
+                  projects.
+                </Trans>
+              </p>
+              <div className="mb-4 text-center ">
+                <LocaleSwitcher />
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   )
 }
 
