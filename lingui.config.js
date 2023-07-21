fcab91fedb17358025c15f620b9b94ba39ef8aec
Milad Nekofar <milad@nekofar.com>
2023-07-21 14:19:30 +0330

    chore: update config files for consistent quote usage
    Refactored the code to use single quotes consistently across lingui.config.js, next.config.js, and .eslintrc.json files. This change was made to adhere to our style guide, which recommends single quotes for a more uniform codebase.

--- lingui.config.js
+++ lingui.config.js
@@ -1,13 +1,13 @@
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-    locales: ["ar", "en", "fa"],
-    sourceLocale: "en",
-    catalogs: [
-        {
-            path: "<rootDir>/src/locales/{locale}/messages",
-            include: ["<rootDir>/src"],
-            exclude: ['**/node_modules/**', '**/.next/**', '**/out/**'],
-        },
-    ],
-    format: "po",
-};
+  locales: ['ar', 'en', 'fa'],
+  sourceLocale: 'en',
+  catalogs: [
+    {
+      path: '<rootDir>/src/locales/{locale}/messages',
+      include: ['<rootDir>/src'],
+      exclude: ['**/node_modules/**', '**/.next/**', '**/out/**'],
+    },
+  ],
+  format: 'po',
+}
