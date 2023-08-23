a61e7085ae488007fd7abb2bc028512bbe7cbcb9
Milad Nekofar <milad@nekofar.com>
2023-08-23 12:40:49 +0330

    refactor: move font utility to styles directory
    The 'fonts' utility was relocated from the 'utils' directory to the 'styles' directory. This change better organizes our codebase by grouping related modules together. The 'fonts' utility is more closely associated with styles, thus this location makes more sense.
    Also, all references to this utility were updated accordingly.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,6 @@
 import clsx, { ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export { vazirmatn } from './fonts'
 export * from './locales'
 
 export const clsxm = (...classes: ClassValue[]) => twMerge(clsx(...classes))
