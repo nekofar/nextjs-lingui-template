d144373879867e30b4f68e968943829ceae811b4
Milad Nekofar <milad@nekofar.com>
2023-08-23 09:16:13 +0330

    chore: update prettier config for better tailwind support
    The `pluginSearchDirs` option was removed and `tailwindFunctions` was introduced in prettier.config.js. This change allows the prettier-plugin-tailwindcss to better detect and format tailwind CSS classes, specifically when using the CLSX or CLSXM npm modules.

--- prettier.config.js
+++ prettier.config.js
@@ -6,5 +6,5 @@ module.exports = {
   tabWidth: 2,
   useTabs: false,
   plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
-  pluginSearchDirs: false,
+  tailwindFunctions: ['clsx', 'clsxm'],
 }
