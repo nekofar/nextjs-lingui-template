fcdbe351cc3d49783c1843913b5a3a60da72e05d
Milad Nekofar <milad@nekofar.com>
2023-08-23 08:53:32 +0330

    refactor: add `clsxm` method for style flexibility
    This commit addresses a refactor in 'src/utils/index.ts' file. The previous exports of 'vazirmatn' and 'locales' were changed into imports to ensure clear code flow. A third-party library, 'clsx', was also imported for concatenating CSS class names. A new method, 'clsxm', was added to merge tailwind classes for increased style flexibility. This change enhances overall usability and improves code readability.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,7 @@
+import clsx, { ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
 export { vazirmatn } from './fonts'
 export * from './locales'
+
+export const clsxm = (...classes: ClassValue[]) => twMerge(clsx(...classes))
