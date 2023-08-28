c25a2e6f2576743be58289b2da566f2b4c0537e8
Milad Nekofar <milad@nekofar.com>
2023-08-28 08:26:30 +0330

    chore: add webpack fallback configuration in `next.config.js`
    This commit updates our next.config.js file by adding a webpack block for the 'fallback' configuration. This is to resolve an issue where browser imports failed due to Node's fs module not being present. By setting 'fs' as false, we ensure the browser bundle ignores node-specific modules."

--- next.config.js
+++ next.config.js
@@ -15,6 +15,18 @@ const nextConfig = {
       ],
     ],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,
+
+      fs: false,
+    }
+
+    // Important: return the modified config
+    return config
+  },
 }
 
 module.exports = nextConfig
